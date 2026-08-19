521e995f252f77eeb35d0e797b25fb3a490db3f6
Belousov Vyacheslav <Mexan1.3.7@yandex.ru>
2026-08-19 14:27:44 +0300

    fix assert

--- tests/test_faq.py
+++ tests/test_faq.py
@@ -9,10 +9,7 @@ from data.config import FAQ_DATA
 )
 def test_faq_answer_is_shown_on_click(driver, question_index, question, expected_answer):
     main_page = MainPage(driver).open()
-    assert not main_page.is_faq_answer_visible(question_index), (f'Ответ на вопрос "{question}" не должен быть виден до клика')
     main_page.click_faq_question(question_index)
-
-    assert main_page.is_faq_answer_visible(question_index), (
+    assert main_page.is_faq_answer_visible(question_index) and main_page.get_faq_answer_text(question_index) == expected_answer, (
         f'Ответ на вопрос "{question}" должен стать видимым после клика'
     )
-    assert main_page.get_faq_answer_text(question_index) == expected_answer
